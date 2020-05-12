2f828f35b8d45d4e36a009370c3c1e710eb1b667
MatheusRogge <matheus-rogge@hotmail.com>
2020-05-12 00:39:26 -0300

    Get script options method

--- lib/wmhub_web/views/client_renderer_view.ex
+++ lib/wmhub_web/views/client_renderer_view.ex
@@ -1,3 +1,9 @@
 defmodule WmhubWeb.ClientRendererView do
     use WmhubWeb, :view
+
+    def get_script_options(assigns) do
+        assigns
+        |> Map.take([:payment_pointer, :project_id, :session_id])
+        |> Jason.encode!()
+    end
 end
