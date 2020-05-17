b2afc7cc636885b9484a993c0e91d5b517c106e8
MatheusRogge <matheus-rogge@hotmail.com>
2020-05-17 02:18:34 -0300

    Added pointer update and minor adjustments

--- assets/js/wmhub.js
+++ assets/js/wmhub.js
@@ -1,43 +1,112 @@
-import { Socket } from 'phoenix';
+/// @ts-check
+import { Socket, Channel } from 'phoenix';
 
+/// @ts-ignore
+const { SERVER_PATH } = process.env;
+
+/**
+ * WM Hub
+ * 
+ * @typedef {{ project_id: string, payment_pointers: [string] }} ProjectOptions
+ */
 function WmHub() {
-    let socket;
-    let projectChannel;
 
-    function setupSocket({ session_id, project_id }) {
-        socket = new Socket('ws://localhost:4000/monetization', {
-            params: {
-                sessionId: session_id 
-            }
+    /**
+     * @type {Socket}
+     */
+    let socketReference;
+
+    /**
+     * @type {Element}
+     */
+    let metatagReference;
+
+    /**
+     * @type {Channel}
+     */
+    let projectChannelReference;
+
+    /**
+     * Creates a socket;
+     * 
+     * @return {Socket}
+     */
+    function setupSocket() {
+        const socket = new Socket(`ws://${SERVER_PATH}/monetization`, {});
+        socket.connect()
+    
+        return socket;
+    }
+
+    /**
+     * 
+     * @param {ProjectOptions} options
+     * @param {Socket} socket 
+     */
+    function setupChannel({ project_id }, socket) {
+        const channel = socket.channel(`monetization:${project_id}`);
+
+        channel.join().receive("error", response => {
+            throw new Error(`Failed to join project channel. Socket details: ${JSON.stringify(response)}`);
         });
 
-        socket.connect();
+        return channel;
+    }
 
-        projectChannel = socket
-            .channel(`monetization:${project_id}`);
-
-        projectChannel
-            .join()
-            .receive("ok", console.log);
-
-        projectChannel.on('pointer-update', ({ pointers }) => {
-            console.log('pointers>>>', pointers);
+    /**
+     * Subscribes to API events.
+     * 
+     * @param {Channel} channel
+     */
+    function subscribePointerEvents(channel) {
+        channel.on('pointer-update', ({ pointers: [paymentPointer] }) => {
+            if (metatagReference && paymentPointer) {
+                metatagReference.setAttribute('content', paymentPointer.pointer);
+            }
         });
     }
 
-    function createMetaTag(paymentPointer) {
+    /**
+     * Creates the web moetization tag on page.
+     * 
+     * @param {ProjectOptions} options
+     */
+    function setupMetaTag({ payment_pointers: [paymentPointer] }) {
         const meta = document.createElement('meta');
         meta.setAttribute('name', 'monetization');
         meta.setAttribute('content', paymentPointer);
+
         document.head.appendChild(meta);
+        
+        return meta;
     }
 
     return {
+        /**
+         * Initializes a new project instance
+         * 
+         * @param {ProjectOptions} options
+         */
         init: options => {
-            setupSocket(options);
-            createMetaTag(options.payment_pointers[0]);
+            try {
+                socketReference = setupSocket();
+                metatagReference = setupMetaTag(options);
+                projectChannelReference = setupChannel(options, socketReference);
+
+                subscribePointerEvents(projectChannelReference);
+            }
+            catch (e) {
+                console.error(e);
+            }
+        },
+        destroy: () => {
+            if (socketReference) {
+                socketReference.disconnect();
+                document.head.removeChild(metatagReference);
+            }
         }
     }
 }
 
+// @ts-ignore
 window.wmhub = new WmHub();
